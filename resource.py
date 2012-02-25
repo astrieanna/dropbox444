f52f21e78ec590bfb1baeec890838e0a3af213f4
Joshua Smith <jsmit272@jhu.edu>
2012-02-25 15:34:53 -0500

    Construct Resource from file path

--- resource.py
+++ resource.py
@@ -1,6 +1,21 @@
+import os
+import os.path as o
+import mimetypes
+import datetime
+
 class Resource:
-    def __init__(self, isDir, path):
-        if(isDir):
-            this.category = "directory"
+    def __init__(self, path):
+        #Should only be called on path that exists
+        assert(o.exists(path))
+        #strip trailing '/' to accomidate basepath
+        path = o.realpath(path).rstrip('/')
+
+        self.category = 'directory' if o.isdir(path) else 'file'
+        self.name = o.basename(path)
+        if(self.category == 'directory'):
+            self.numItems = len(os.listdir(path))
         else:
-            this.category = "file"
+            self.size = o.getsize(path)
+        self.relativePath = path
+        self.resourceDate = datetime.datetime.utcfromtimestamp(o.getmtime(path))
+        self.resourceType = mimetypes.guess_type(path)[0]
