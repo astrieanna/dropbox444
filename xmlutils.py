3ab8bf2702d294022f177736adfee119f548dfb9
Leah Hanson <astrieanna@gmail.com>
2012-02-25 14:48:27 -0500

    defined API for xmlutils

--- xmlutils.py
+++ xmlutils.py
@@ -1,2 +1,17 @@
 import xml.etree.ElementTree as ET
 
+# parseResourceList :: String -> [{String:Value}]
+
+# buildResourceList :: [Resource] -> String
+
+# parseResourceDownload :: String -> {String:Value}
+
+# buildResourceDownload :: Resource -> String
+
+# parseResourceUpload :: String -> {String:Value}
+
+# buildResourceDownload :: Resource -> String
+
+#HELPERS:
+# parseResource :: String -> Resource
+# buildResource :: Resource -> String
